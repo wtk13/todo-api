a37d3de4973aed3b37f6c60de12650d2bc8f24e2
Wojtek <rybinski05@gmail.com>
2019-02-04 23:28:00 +0100

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 mongoose.set('useFindAndModify', false);
 
@@ -111,6 +112,10 @@ app.post('/users', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+   res.send(req.user);
 });
 
 app.listen(port, () => {
